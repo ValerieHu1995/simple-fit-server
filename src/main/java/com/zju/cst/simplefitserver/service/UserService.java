607a25b5f13720ce89e6392bc2c4d394a672102b
KoonChen <zpkoon@gmail.com>
2018-11-07 20:24:24 +0800

    🎉 init project

---
+++ src/main/java/com/zju/cst/simplefitserver/service/UserService.java
@@ -0,0 +1,13 @@
+package com.zju.cst.simplefitserver.service;
+
+import com.zju.cst.simplefitserver.model.InfoUser;
+
+public interface UserService {
+
+    public InfoUser getUserInfoByName(String name);
+
+    int insertSelective(InfoUser record);
+
+    int updateByPrimaryKeySelective(InfoUser record);
+
+}

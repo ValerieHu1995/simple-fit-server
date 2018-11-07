c5bd5177010db91e10ff19d879538d5fc0ee166e
KoonChen <zpkoon@gmail.com>
2018-11-07 10:01:35 +0800

    🚧 change sth

--- src/main/java/com/company/project/dao/LogLoginMapper.java
+++
@@ -1,7 +0,0 @@
-package com.company.project.dao;
-
-import com.company.project.core.Mapper;
-import com.company.project.model.LogLogin;
-
-public interface LogLoginMapper extends Mapper<LogLogin> {
-}
--- src/main/java/com/company/project/dao/RelationLessonCredentialMapper.java
+++
@@ -1,7 +0,0 @@
-package com.company.project.dao;
-
-import com.company.project.core.Mapper;
-import com.company.project.model.RelationLessonCredential;
-
-public interface RelationLessonCredentialMapper extends Mapper<RelationLessonCredential> {
-}
